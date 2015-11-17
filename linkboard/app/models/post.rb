aa90a907fc2879ffafbfa5b05c95b11b5cc3814f
Klaney <Keatonus@gmail.com>
2015-11-16 20:33:11 -0800

    POsts works

--- linkboard/app/models/post.rb
+++ linkboard/app/models/post.rb
@@ -2,11 +2,21 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   validates :title,
-  presence: true
+  presence: true,
+  length: { 
+  	minimum: 10,
+  	maximum: 100
+  }
 
   validates :link,
   presence: true,
   :url => true
+
+  validates :name,
+  presence: true,
+  length: {
+  	maximum: 20
+  }
 
 
 end
