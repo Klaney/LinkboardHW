30c226a81291ac24fc42311f99947f4ae653ead7
Klaney <Keatonus@gmail.com>
2015-11-16 22:10:04 -0800

    Here it is

--- linkboard/app/controllers/session_controller.rb
+++ linkboard/app/controllers/session_controller.rb
@@ -1,10 +1,30 @@
 class SessionController < ApplicationController
   def new
+
   end
 
   def create
+  	user = User.authenticate login_params['email'], login_params['password']
+
+		if user
+			#do some stuff
+			session[:user_id] = user.id
+			flash[:success] = "#{user.email} has logged in!"
+			redirect_to login_path
+		else
+			#give an error
+			flash[:danger] = 'You had the wrong email/password'
+			redirect_to login_path
+		end
   end
 
   def destroy
+  	session[:user_id] = nil
+	redirect_to root_path
   end
+  private
+
+	def login_params
+		params.require(:user).permit(:email, :password)
+	end
 end
